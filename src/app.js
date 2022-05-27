9c710127db622a2bc3b27c902f211ef9b17a5592
Smriti Singh <smriti2815@gmail.com>
2022-05-28 03:24:26 +0530

    fix

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const ngoUser = require('./models/ngo');
 
 const app = express();
 
-const port = process.env.port||3000;
+const port = process.env.PORT||3000;
 
 // Defining Paths
 const publicDirectoryPath = path.join(__dirname, '../public');
