9a22de1a129b96806d15e9c5605b26268e50b8e2
VibhutiMandral <52248877+VibhutiMandral@users.noreply.github.com>
2022-05-28 15:48:40 +0530

    responsive changes

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect('mongodb://127.0.0.1:27017/test',{
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-// });
-
-const connectionString = "mongodb+srv://hungerfree:hungerfree4@cluster0.okztt.mongodb.net";
-mongoose.connect(connectionString+'/users',{
+mongoose.connect('mongodb://127.0.0.1:27017/test',{
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
+
+// const connectionString = "mongodb+srv://hungerfree:hungerfree4@cluster0.okztt.mongodb.net";
+// mongoose.connect(connectionString+'/users',{
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// });
